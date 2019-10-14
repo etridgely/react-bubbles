cbc660a5cf4bdc979ef1e92bdd07a19971c35667
etridgely <5757423+etridgely@users.noreply.github.com>
2019-10-13 19:07:55 -0700

    working on login and BubblePage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,48 @@
 import React from "react";
+import axios from "axios";
+import { Form } from 'semantic-ui-react';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState({username: '', password: ''});
+  
+  const updateLogin = e => {
+    setLogin({ ...login, [e.target.name]: e.target.value});
+  };
+
+  const logUser = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', login)
+      .then(res => {
+        localStorage.setItem('userToken', res.data.payload);
+        props.history.push('/bubble');
+      })
+      .catch(err => console.log(err.response));
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <Form onSubmit = {logUser}>
+        <Form.Group>
+          <Form.Input
+            placeholder="Username"
+            name="username"
+            value={login.username} 
+            onChange={updateLogin}
+          />
+          <Form.Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={login.username}
+            onChange={updateLogin}
+          />
+          <Form.Button content="Log in" />
+        </Form.Group>
+      </Form>
+      
+    </div>
   );
 };
 
